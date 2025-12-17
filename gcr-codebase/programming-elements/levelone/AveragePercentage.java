81cab4f807bc956202feb6a35a7a575537217c48
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-17 14:30:58 +0530

    [Priyanshu] : added code that finds AveragePercentage

---
+++ gcr-codebase/programming-elements/levelone/AveragePercentage.java
@@ -0,0 +1,12 @@
+class AveragePercentage{
+public static void main(String args[]){
+
+int maths=94;
+int physics=95;
+int chemistry=96;
+
+double averagePercentage= (maths+physics+chemistry)/3;
+
+System.out.println("Sam’s average mark in PCM is "+ averagePercentage);
+}
+}
