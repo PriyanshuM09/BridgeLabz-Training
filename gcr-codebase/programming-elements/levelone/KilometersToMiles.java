7c6145b2e0a41a93079b213fa0a80d8c49d2f833
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-17 14:42:36 +0530

    [Priyanshu] :  Added code that converts Kilometers to miles

---
+++ gcr-codebase/programming-elements/levelone/KilometersToMiles.java
@@ -0,0 +1,9 @@
+class kilometersToMiles{
+public static void main(String args[]){
+
+double kilometers=10.8;
+double miles= kilometers * 0.6;
+
+System.out.println("The distance "+kilometers+" km in miles is "+miles);
+}
+}
