ebfdc9e3c04d21d626ca70c95691f540cbd429db
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-17 15:21:06 +0530

    [Priyanshu] : Added code for fee discount

---
+++ gcr-codebase/programming-elements/levelone/FeeDiscount.java
@@ -0,0 +1,12 @@
+class FeeDiscount{
+public static void main(String args[]){
+
+int fee=125000;
+int discountPercentage= 10;
+int discount=125000/10;
+
+int finalFee=fee-discount;
+
+System.out.println("The discount amount is INR "+discount+" and final discounted fee is INR "+finalFee);
+}
+}
