d363242653a8104892cca71bdc4d17fa4e95975a
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-18 15:02:35 +0530

    [Priyanshu] : Added code for Rocket launch countdown

---
+++ gcr-codebase/control-flows/level-one-practice-programs/RocketLaunch2.java
@@ -0,0 +1,19 @@
+// Rewrite program 8 to do the countdown using the for-loop
+import java.util.Scanner;
+
+public class RocketLaunch2{
+	public static void main(String args[]){
+		
+		Scanner sc= new Scanner(System.in);
+		
+		//Taking input for countdown
+		int counter= sc.nextInt();
+		
+		// Using for loop for countdown
+		for(int i=counter; i>=0;i--){
+		System.out.print(counter+" ");
+		   
+		}
+		}
+		
+	}
