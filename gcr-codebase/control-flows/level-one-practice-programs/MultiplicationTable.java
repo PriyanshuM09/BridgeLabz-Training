68b1d25b9708689d95e0fd64f55d046b26403a9e
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-19 00:00:09 +0530

    [Priyanshu] : Added code for multiplication of a number btw 6 to 9

---
+++ gcr-codebase/control-flows/level-one-practice-programs/MultiplicationTable.java
@@ -0,0 +1,17 @@
+//Create a program to find the multiplication table of a number entered by the user from 6 to 9
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take input from user
+        int number = sc.nextInt();
+
+        // Print multiplication table from 6 to 9
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
