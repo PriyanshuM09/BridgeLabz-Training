2dcbd112f87cf019064ef282dbf1c12dd41de2fd
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-18 11:21:14 +0530

    [Priyanshu] : Added code FahrenhiteConversion

---
+++ gcr-codebase/program-prerequisite/FahrenhiteConversion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class FahrenhiteConversion{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+int celcius=sc.nextInt();
+int fahrenhite=(celcius * 9/5)+32;
+System.out.println(fahrenhite);
+}}
