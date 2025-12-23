e8b8199e8275502d937fc1ad03a8ecda15104558
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-24 02:02:45 +0530

    [Priyanshu] : Added code palindrome check

---
+++ gcr-codebase/extras/string-practice-problems/PalindromeCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PalindromeCheck{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+      System.out.print("Enter a string ");
+        String text = sc.nextLine();
+
+        String reversed = "";
+
+  for(int i=text.length()- 1;i >=0; i--){
+      reversed = reversed + text.charAt(i);
+        }
+        if (text.equalsIgnoreCase(reversed)) {
+            System.out.println("The string is a Palindrome");
+        } 
+	   else {
+            System.out.println("The string is NOT a Palindrome");
+       }
+    }
+}
