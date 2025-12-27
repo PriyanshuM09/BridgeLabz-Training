66a17fd1b45036782956f2374f33b71681d63828
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-27 17:08:30 +0530

    [Priyanshu] : Added main method for class circle

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/CircleMain.java
@@ -0,0 +1,15 @@
+package com.constructors.levelone;
+
+public class CircleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        Circle c1 = new Circle();
+        c1.displayRadius();
+
+        Circle c2 = new Circle(5.5);
+        c2.displayRadius();
+	}
+
+}
