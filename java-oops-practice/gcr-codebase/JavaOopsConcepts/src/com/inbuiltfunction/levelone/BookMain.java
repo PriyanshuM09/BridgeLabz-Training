5cc6d140c25116bbc4bf429b08e9eff74a8f1855
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-30 00:48:30 +0530

    [Priyanshu] : Added main method Book main for class book

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inbuiltfunction/levelone/BookMain.java
@@ -0,0 +1,19 @@
+package com.inbuiltfunction.levelone;
+
+public class BookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      // Set the library name (static variable)
+	       Book.setLibraryName("Egmore Library");
+	       // Display the library name
+	       Book.displayLibraryName();
+	       // Create a new book instance
+	       Book book1 = new Book("Effective Java", "Joshua Bloch", "978-0134685991");
+	       // Display book details
+	       book1.displayBookDetails();
+	   }
+
+	}
+
+
