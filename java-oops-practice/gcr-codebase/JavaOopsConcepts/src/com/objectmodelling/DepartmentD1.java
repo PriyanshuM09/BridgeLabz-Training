40255378180c8605959bffbcb99d51c4948ac05b
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-31 01:32:15 +0530

    [Priyanshu] : Added code for Department d1 class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/DepartmentD1.java
@@ -0,0 +1,13 @@
+package com.objectmodelling;
+
+public class DepartmentD1 {
+	   private String deptName;
+
+	    public DepartmentD1(String deptName) {
+	        this.deptName = deptName;
+	    }
+
+	    public void showDepartment() {
+	        System.out.println("Department: " + deptName);
+	    }
+}
