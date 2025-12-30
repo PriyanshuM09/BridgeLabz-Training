b3609dbe2f38a9fb29b73414c3e45f154bd8a5d8
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-31 01:21:48 +0530

    [Priyanshu] : Added class Account

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/Account.java
@@ -0,0 +1,25 @@
+package com.objectmodelling;
+
+public class Account {
+	  private int accountNumber;
+	    private double balance;
+	    private Bank bank;
+
+	    public Account(int accountNumber, double balance, Bank bank) {
+	        this.accountNumber = accountNumber;
+	        this.balance = balance;
+	        this.bank = bank;
+	    }
+
+	    public double getBalance() {
+	        return balance;
+	    }
+
+	    public int getAccountNumber() {
+	        return accountNumber;
+	    }
+
+	    public Bank getBank() {
+	        return bank;
+	    }
+}
