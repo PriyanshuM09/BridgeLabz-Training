8d01d863b6492736d011e7e5db942b83f6caeed0
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2025-12-30 15:56:13 +0530

    [Priyanshu] : Added code for temprature logger

---
+++ scenario-based-codespace/TempratureLogger.java
@@ -0,0 +1,27 @@
+// Temperature Logger 
+
+import java.util.Scanner;
+public class TempratureLogger{
+public static void main(String args[]){
+ Scanner sc= new Scanner(System.in);
+ int arr[]=new int[7];
+ 
+ for(int i=0;i<7;i++){
+ arr[i]= sc.nextInt();
+ }
+ 
+ double avg;
+ int tot=0;
+ int maxTemprature= Integer.MIN_VALUE;
+ 
+ for(int i=0;i<arr.length;i++){
+   if(arr[i]>maxTemprature){
+    maxTemprature=arr[i];
+   }
+   tot+=arr[i];
+  }
+  avg= tot/7;
+  System.out.println("Max tempature is : "+ maxTemprature);
+  System.out.println("Average is : "+avg);
+}
+}
