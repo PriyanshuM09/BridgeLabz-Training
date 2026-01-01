60bd4bc0ddb168b8d7190bbb827b4b523ed15432
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-01 15:59:48 +0530

    [Priyanshu] : Added code for device class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/singleinheritence/Device.java
@@ -0,0 +1,17 @@
+package com.inheritance.singleinheritence;
+
+public class Device {
+
+    int deviceId;
+    String status;
+
+    Device(int deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
