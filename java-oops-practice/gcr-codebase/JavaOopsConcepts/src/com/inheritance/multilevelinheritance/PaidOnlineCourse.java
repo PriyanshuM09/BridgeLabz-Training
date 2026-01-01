876b482c86d4f67369680e4a6ec78c12f6c6686a
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-01 16:24:53 +0530

    [Priyanshu] : Added code for Paid online Course

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/PaidOnlineCourse.java
@@ -0,0 +1,22 @@
+package com.inheritance.multilevelinheritance;
+
+public class PaidOnlineCourse extends OnlineCourse {
+
+    double fee;
+    double discount;
+
+    PaidOnlineCourse(String courseName, int duration, String platform,
+                     boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Course Fee: " + fee);
+        System.out.println("Discount: " + discount + "%");
+    }
+}
+
