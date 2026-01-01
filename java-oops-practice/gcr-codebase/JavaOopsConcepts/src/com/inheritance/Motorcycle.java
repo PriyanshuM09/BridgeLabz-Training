613eeb3015112084515abbb519c9d198a221a20a
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-01 15:35:34 +0530

    [Priyanshu] : Added code for motorcycle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/Motorcycle.java
@@ -0,0 +1,17 @@
+package com.inheritance;
+
+public class Motorcycle extends Vehicle {
+
+    private boolean hasGear;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasGear) {
+        super(maxSpeed, fuelType);
+        this.hasGear = hasGear;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Gear: " + hasGear);
+    }
+}
