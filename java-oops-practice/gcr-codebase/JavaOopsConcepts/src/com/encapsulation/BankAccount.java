ea9313e7a65073b8e5acee9b3289f9ef7e9861d1
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-02 13:27:24 +0530

    [priyanshu] : Added code for BankAccount class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/BankAccount.java
@@ -0,0 +1,44 @@
+package com.encapsulation;
+
+public abstract class BankAccount {
+
+    private int accountNumber;
+    private String holderName;
+    private double balance;
+
+    public BankAccount(int accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Encapsulation: getters
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Concrete methods
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    // Abstract method
+    public abstract double calculateInterest();
+}
+
