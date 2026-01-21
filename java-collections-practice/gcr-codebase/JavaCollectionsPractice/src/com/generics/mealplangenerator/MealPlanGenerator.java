5a2955336890f1c79091acc9574bd84a58b20bae
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-21 16:08:59 +0530

    [Priyanshu] : Added code for mealplangenerator

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/generics/mealplangenerator/MealPlanGenerator.java
@@ -0,0 +1,11 @@
+package com.generics.mealplangenerator;
+class MealPlanGenerator {
+
+    public static <T extends MealPlan> Meal<T> generateMealPlan(T mealPlan) {
+        System.out.println(" meal plan : " + mealPlan.getMealType());
+        if (mealPlan.getCalories() <= 0){
+         throw new IllegalArgumentException("Invalid calorie value");
+        }
+        return new Meal<>(mealPlan);
+    }
+}
