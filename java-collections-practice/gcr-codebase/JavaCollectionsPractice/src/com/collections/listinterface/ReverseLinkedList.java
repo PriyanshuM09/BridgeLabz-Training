b00b0ad22ef3a772ec77e57fa222531f54cdde1f
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-23 13:30:15 +0530

    [Priyanshu] : Added code for ReverseLinkedList

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/listinterface/ReverseLinkedList.java
@@ -0,0 +1,31 @@
+package com.collections.listinterface;
+import java.util.LinkedList;
+import java.util.Arrays;
+public class ReverseLinkedList {
+
+	 public static void reverseLinkedList(LinkedList<Integer> list) {
+
+	        int left = 0;
+	        int right = list.size() - 1;
+
+	        while (left < right) {
+	            Integer temp = list.get(left);
+	            list.set(left, list.get(right));
+	            list.set(right, temp);
+
+	            left++;
+	            right--;
+	        }
+	    }
+
+	    public static void main(String[] args) {
+
+	        LinkedList<Integer> list = new LinkedList<>(
+	                Arrays.asList(1, 2, 3, 4, 5)
+	        );
+
+	        reverseLinkedList(list);
+	        System.out.println(list);
+	    }
+}
+
