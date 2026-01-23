10efde432a480ff29cd91af937d7500d9fad2cdb
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-23 15:31:28 +0530

    [Priyanshu]  : Added code forSetSortedList

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/setinterface/SetSortedList.java
@@ -0,0 +1,20 @@
+package com.collections.setinterface;
+import java.util.*;
+
+public class SetSortedList {
+    public static void main(String[] args) {
+
+        // Creating HashSet
+        Set<Integer> set = new HashSet<>();
+        set.add(5);
+        set.add(3);
+        set.add(9);
+        set.add(1);
+
+        // Convert Set to List
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        System.out.println("Input Set: " + set);
+        System.out.println("Sorted List: " + list);
+    }
+}
