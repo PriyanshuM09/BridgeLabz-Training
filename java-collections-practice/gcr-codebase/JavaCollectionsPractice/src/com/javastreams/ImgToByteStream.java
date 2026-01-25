b8e503881d05faa95546e77ebbd6fafbfcb47e72
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-26 02:35:35 +0530

    [Priyanshu] : Added code for ImgToByteStream

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/javastreams/ImgToByteStream.java
@@ -0,0 +1,36 @@
+package com.javastreams;
+import java.io.*;
+public class ImgToByteStream{
+	    public static void main(String[] args) {
+	    String sourceImage = "D:/InputFiles/original.jpg";
+	        String outputImage = "D:/OutputFiles/copied.jpg";
+	        try{
+	            FileInputStream fis = new FileInputStream(sourceImage);
+	            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	            byte[] buffer = new byte[4096];
+	            int bytesRead;
+
+	            while((bytesRead = fis.read(buffer)) != -1) {
+	                baos.write(buffer, 0, bytesRead);
+	            }
+	            byte[] imageBytes = baos.toByteArray();
+	            ByteArrayInputStream bais = new ByteArrayInputStream(imageBytes);
+	            FileOutputStream fos = new FileOutputStream(outputImage);
+
+	            while ((bytesRead = bais.read(buffer)) != -1) {
+	                fos.write(buffer, 0, bytesRead);
+	            }
+	            fis.close();
+	            baos.close();
+	            bais.close();
+	            fos.close();
+
+	            System.out.println("Image copied successfully.");
+
+	        } catch (IOException e) {
+	            System.out.println("Error while processing image.");
+	        }
+	    }
+	}
+
+
