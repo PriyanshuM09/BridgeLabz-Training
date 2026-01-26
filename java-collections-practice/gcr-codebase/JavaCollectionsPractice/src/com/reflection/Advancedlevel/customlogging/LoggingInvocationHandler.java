d75ee2d60a1c59955387f292cc3632e200499f8b
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-27 01:00:11 +0530

    [Priyanshu] : Added code customlogging

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/Advancedlevel/customlogging/LoggingInvocationHandler.java
@@ -0,0 +1,19 @@
+package com.reflection.Advancedlevel.customlogging;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class LoggingInvocationHandler implements InvocationHandler {
+
+    private final Object target;
+
+    public LoggingInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("Calling method: " + method.getName());
+
+        return method.invoke(target, args);
+    }
+}
