e8c7656b05f8615939b708ab9d083fcdcfafd336
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-28 17:12:49 +0530

    [Priyanshu] : Added code for filter

---
+++ java-io-streams-practice/gcr-codebase/JavaIOStreams/src/com/json/filter/FilterUsersByAge.java
@@ -0,0 +1,36 @@
+package com.json.filter;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+public class FilterUsersByAge {
+
+    public static void main(String[] args) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream is = FilterUsersByAge.class
+                    .getClassLoader()
+                    .getResourceAsStream("users.json");
+
+            if (is == null) {
+                throw new RuntimeException("users.json not found");
+            }
+            JsonNode rootNode = mapper.readTree(is);
+
+            System.out.println("Users older than 25:");
+
+            Iterator<JsonNode> iterator = rootNode.elements();
+            while (iterator.hasNext()) {
+                JsonNode user = iterator.next();
+
+                if (user.has("age") && user.get("age").asInt() > 25) {
+                    System.out.println(user);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
