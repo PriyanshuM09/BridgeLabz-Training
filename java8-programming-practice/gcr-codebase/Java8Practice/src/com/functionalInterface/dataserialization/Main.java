1bec10a0262a32daf4b78b2c682fc657134dc494
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-30 17:15:44 +0530

    [Priyanshu] : Added code of dataserialization

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/functionalInterface/dataserialization/Main.java
@@ -0,0 +1,12 @@
+package com.functionalInterface.dataserialization;
+
+public class Main {
+	    public static void main(String[] args) {
+
+	        Customer customer = new Customer(1, "Suyash");
+	    
+	        BackupProcessor.backupObject(customer); 
+	    }
+	}
+
+
