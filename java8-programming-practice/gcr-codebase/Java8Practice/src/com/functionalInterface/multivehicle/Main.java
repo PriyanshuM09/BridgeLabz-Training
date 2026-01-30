5f0164a640d48e03a2ee992a47310b23d3c642cc
PriyanshuM09 <mishrapriyanshu436@gmail.com>
2026-01-30 17:16:18 +0530

    [Priyanshu] : Added code of multivehicle

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/functionalInterface/multivehicle/Main.java
@@ -0,0 +1,20 @@
+package com.functionalInterface.multivehicle;
+
+public class Main {
+
+	    public static void main(String[] args) {
+
+	        RentalVehicle car=new Car();
+	        RentalVehicle bike=new Bike();
+
+	        car.rent();
+	        bike.rent();
+	    
+
+	        car.returnVehicle();
+	        bike.returnVehicle();
+	    
+	    }
+	}
+
+
